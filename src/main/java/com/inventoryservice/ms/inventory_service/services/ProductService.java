3769ef563c053417b0971fbfe727afc35f0665a5
ruanvalente <ruanopensuselinux@gmail.com>
2025-04-16 20:12:01 -0300

    feat: Implement findById method in ProductService for retrieving products by ID

--- src/main/java/com/inventoryservice/ms/inventory_service/services/ProductService.java
+++ src/main/java/com/inventoryservice/ms/inventory_service/services/ProductService.java
@@ -1,10 +1,12 @@
 package com.inventoryservice.ms.inventory_service.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.inventoryservice.ms.inventory_service.entities.Product;
+import com.inventoryservice.ms.inventory_service.exceptions.ProductNotFoundException;
 import com.inventoryservice.ms.inventory_service.repositories.ProductRepository;
 
 @Service
@@ -17,5 +19,10 @@ public class ProductService {
 
   public List<Product> listAll() {
     return this.productRepository.findAll();
+  }
+
+  public Product findById(Long id) {
+    return productRepository.findById(id)
+        .orElseThrow(() -> new ProductNotFoundException(id));
   }
 }
