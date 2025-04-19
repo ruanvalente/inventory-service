eb98782c8102c9d5e26642e4ee2efe50a0211b03
ruanvalente <ruanopensuselinux@gmail.com>
2025-04-19 10:40:58 -0300

    feat: Enhance RabbitMQ configuration and listener to process messages with DTOs

--- src/main/java/com/inventoryservice/ms/inventory_service/config/RabbitMqConfig.java
+++ src/main/java/com/inventoryservice/ms/inventory_service/config/RabbitMqConfig.java
@@ -4,6 +4,9 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,5 +29,19 @@ public class RabbitMqConfig {
         .bind(inventoryQueue())
         .to(inventoryExchange())
         .with("inventory-routing-key");
+  }
+
+  @Bean
+  public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setMessageConverter(messageConverter());
+    factory.setErrorHandler(t -> System.err.println("Listener error: " + t.getMessage()));
+    return factory;
+  }
+
+  @Bean
+  public Jackson2JsonMessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
   }
 }
