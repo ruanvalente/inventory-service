6dfb98ab7803510d168e3fc788efda651bae5496
ruanvalente <ruanopensuselinux@gmail.com>
2025-05-01 11:59:24 -0300

    refactor: Change errorType field in ErrorMessageDTO to use ErrorType enum

--- src/main/java/com/inventoryservice/ms/inventory_service/entities/dto/response/ErrorMessageDTO.java
+++ src/main/java/com/inventoryservice/ms/inventory_service/entities/dto/response/ErrorMessageDTO.java
@@ -3,11 +3,12 @@ package com.inventoryservice.ms.inventory_service.entities.dto.response;
 import java.time.Instant;
 
 import com.inventoryservice.ms.inventory_service.entities.dto.request.OrderRequestDTO;
+import com.inventoryservice.ms.inventory_service.entities.enums.ErrorType;
 
 public record ErrorMessageDTO(
-    String errorType, // "PRODUCT_NOT_FOUND", "INSUFFICIENT_STOCK", "GENERIC_ERROR"
-    String message,
-    OrderRequestDTO orderRequest,
-    Instant timestamp) {
+        ErrorType errorType,
+        String message,
+        OrderRequestDTO orderRequest,
+        Instant timestamp) {
 
 }
