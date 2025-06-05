0975a0b2c1e6833460b2a90597b9f8df23c2a311
ruanvalente <contato.ruanvalente@gmail.com>
2025-06-04 21:12:34 -0300

    feat: Add validation exception handler for MethodArgumentNotValidException

--- src/main/java/com/inventoryservice/ms/inventory_service/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/inventoryservice/ms/inventory_service/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.inventoryservice.ms.inventory_service.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -33,6 +34,26 @@ public class GlobalExceptionHandler {
         "Bad Request",
         ex.getMessage(),
         request.getRequestURI());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+  }
+
+  @SuppressWarnings("null")
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ApiError> handleValidationException(
+      MethodArgumentNotValidException ex,
+      HttpServletRequest request) {
+
+    String message = ex.getBindingResult().getFieldErrors().stream()
+        .map(error -> error.getField() + ": " + error.getDefaultMessage())
+        .reduce((msg1, msg2) -> msg1 + "; " + msg2)
+        .orElse("Parâmetros obrigatórios ausentes ou inválidos.");
+
+    ApiError error = new ApiError(
+        HttpStatus.BAD_REQUEST.value(),
+        "Bad Request",
+        message,
+        request.getRequestURI());
+
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
   }
 }
