4001a181c23b2df92f4f1fad2d7c8370679bf7e9
ruanvalente <contato.ruanvalente@gmail.com>
2025-06-04 21:11:58 -0300

    feat: Update ProductController to include API versioning and validation for product creation

--- src/main/java/com/inventoryservice/ms/inventory_service/controllers/ProductController.java
+++ src/main/java/com/inventoryservice/ms/inventory_service/controllers/ProductController.java
@@ -3,16 +3,20 @@ package com.inventoryservice.ms.inventory_service.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inventoryservice.ms.inventory_service.entities.Product;
+import com.inventoryservice.ms.inventory_service.entities.dto.request.ProductRequestDTO;
 import com.inventoryservice.ms.inventory_service.services.ProductService;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
   private final ProductService productService;
@@ -35,5 +39,11 @@ public class ProductController {
   @GetMapping("/{id}")
   public ResponseEntity<Product> findById(@PathVariable Long id) {
     return ResponseEntity.ok(productService.findById(id));
+  }
+
+  @PostMapping
+  public ResponseEntity<Product> create(@Validated @RequestBody ProductRequestDTO productDTO) {
+    Product createdProduct = productService.create(productDTO);
+    return ResponseEntity.status(201).body(createdProduct);
   }
 }
