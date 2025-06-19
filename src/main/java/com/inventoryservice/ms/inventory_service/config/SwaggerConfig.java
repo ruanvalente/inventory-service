8b06104cf6304648ca118767b5711cf46bef8e6f
ruanvalente <contato.ruanvalente@gmail.com>
2025-06-19 19:21:23 -0300

    feat: Implement Swagger configuration for API documentation

---
+++ src/main/java/com/inventoryservice/ms/inventory_service/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.inventoryservice.ms.inventory_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Inventory Service API")
+                        .version("1.0.0")
+                        .description("API for managing inventory and products"));
+    }
+}
