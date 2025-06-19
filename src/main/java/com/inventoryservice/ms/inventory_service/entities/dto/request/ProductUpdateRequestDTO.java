7ea4cb9752bb4d58237510eb6a0636963165d8fa
ruanvalente <contato.ruanvalente@gmail.com>
2025-06-19 18:57:19 -0300

    refactor: Remove availableQuantity field from ProductUpdateRequestDTO

--- src/main/java/com/inventoryservice/ms/inventory_service/entities/dto/request/ProductUpdateRequestDTO.java
+++ src/main/java/com/inventoryservice/ms/inventory_service/entities/dto/request/ProductUpdateRequestDTO.java
@@ -1,10 +1,8 @@
 package com.inventoryservice.ms.inventory_service.entities.dto.request;
 
 public record ProductUpdateRequestDTO(
-    String name,
-    String description,
-    Integer availableQuantity,
-    Double price
-) {
+        String name,
+        String description,
+        Double price) {
 
 }
