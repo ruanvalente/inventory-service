22ccb78022afdfbbc33574631040d9d222c31948
ruanvalente <contato.ruanvalente@gmail.com>
2025-06-20 19:40:03 -0300

    chore: move ProductServiceTest to service package

--- src/test/java/com/inventoryservice/ms/inventory_service/services/ProductServiceTest.java
+++ src/test/java/com/inventoryservice/ms/inventory_service/services/ProductServiceTest.java
@@ -1,4 +1,4 @@
-package com.inventoryservice;
+package com.inventoryservice.ms.inventory_service.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
